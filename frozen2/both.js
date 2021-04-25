00b951e17f8c05c54a1dd189323e50331254b7af
selambenhorhor <83166798+selambenhorhor@users.noreply.github.com>
2021-04-25 17:51:55 +0300

    Add files via upload

---
+++ frozen2/both.js
@@ -0,0 +1,19 @@
+const meme = require("themememaker");
+const db = require("wio.db");
+const dc = require("discord.js")
+exports.run = async (client, message, args) => {
+if(!args[0]) return message.channel.send("Komut kullanım örneği: `+both sen%20ordaki bana bak\n**Üst Taraf İçin Boşluk Kullanmayın Boşluk Yerine %20 yazın.**")
+let ust = args[0]
+let q = args.slice(1).join(" ");
+let alt = q.split(" ").join("%20")
+console.log(alt)
+console.log(ust)
+db.add(`toplam2_`, 1)
+let res = await meme.make("both", ust, alt)
+    console.log(res)
+    const e = new dc.MessageEmbed()
+    .addField("Yaptığın Meme", res.response)
+    .setColor("GREEN")
+    .setImage(res.response)
+    message.channel.send(e)
+  };
