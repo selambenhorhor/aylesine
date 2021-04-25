600c6e3ce5fce626b07ec50c5d6f13052400769a
selambenhorhor <83166798+selambenhorhor@users.noreply.github.com>
2021-04-25 17:34:00 +0300

    Add files via upload

---
+++ lrows/ready.js
@@ -0,0 +1,7 @@
+module.exports = (client) => {
+const Discord = require("discord.js");
+let config = require("../config.json");
+	console.log("Bot Giriş Yaptı");
+	client.user.setActivity("+yardım | Lrows & Fozen");
+
+}
